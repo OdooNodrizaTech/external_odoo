baa4c6f952922d25d37cda078ae0400388f71b77
Informatica Arelux <informatica@arelux.com>
2020-05-21 14:46:58 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-728

--- external_odoo_base_arelux/__manifest__.py
+++ external_odoo_base_arelux/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'External Odoo Base Arelux',
-    'version': '12.0.1.0.0',    
+    'version': '12.0.1.0.0',
     'author': 'Odoo Nodriza Tech (ONT)',
     'website': 'https://nodrizatech.com/',
     'category': 'Tools',
     'license': 'AGPL-3',
-    'depends': ['external_odoo_base', 'arelux_partner_questionnaire'],
+    'depends': ['external_odoo_base', 'arelux_partner_questionnaire', 'stock', 'sale'],
     'data': [
         'views/external_source.xml',
     ],
     'installable': True,
-    'auto_install': False,    
+    'auto_install': False,
 }
--- external_odoo_base_arelux/models/__init__.py
+++ external_odoo_base_arelux/models/__init__.py
@@ -2,4 +2,5 @@
 from . import external_customer
 from . import external_sale_order
 from . import external_source
-from . import external_stock_picking 
+from . import external_stock_picking
+from . import sale_order
