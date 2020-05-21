becd4157f239628d4ffbe6a361e00bacc33fb336
Informatica Arelux <informatica@arelux.com>
2020-05-21 12:42:40 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-728

--- external_odoo_base/models/sale_order.py
+++ external_odoo_base/models/sale_order.py
@@ -11,3 +11,20 @@ class SaleOrder(models.Model):
         comodel_name='external.sale.order',
         string='External Sale Order Id'
     )
+
+    @api.multi
+    def action_confirm(self):
+        return_action = super(SaleOrder, self).action_confirm()
+        # operations
+        for item in self:
+            if item.state == 'sale':
+                if item.external_sale_order_id.id > 0:
+                    if item.external_sale_order_id.external_source_id.id > 0:
+                        for picking_id in item.picking_ids:
+                            if picking_id.picking_type_id.id != item.external_sale_order_id.external_source_id.external_sale_order_picking_type_id.id:
+                                picking_id.picking_type_id = item.external_sale_order_id.external_source_id.external_sale_order_picking_type_id.id
+                                picking_id.name = self.env['ir.sequence'].next_by_code(
+                                    self.env['stock.picking.type'].search([('id', '=', picking_id.picking_type_id.id)])[
+                                        0].sequence_id.code)
+        # return
+        return return_action
