4c8fc271633e51f00f5d0211cfda3a3047bfee47
Informatica Arelux <informatica@arelux.com>
2020-06-30 11:34:59 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-887

--- external_odoo_base_arelux/models/shipping_expedition.py
+++ external_odoo_base_arelux/models/shipping_expedition.py
@@ -9,12 +9,12 @@ class ShippingExpedition(models.Model):
     @api.model
     def create(self, values):
         return_object = super(ShippingExpedition, self).create(values)
-        #operations
-        if return_object.user_id.id==0:
-            if return_object.picking_id.id>0:
-                if return_object.picking_id.external_stock_picking_id.id>0:
-                    if external_stock_picking_id.external_stock_picking_id.external_source_id.id>0:
-                        if external_stock_picking_id.external_stock_picking_id.external_source_id.external_stock_picking_user_id.id>0:
-                            return_object.user_id = external_stock_picking_id.external_stock_picking_id.external_source_id.external_stock_picking_user_id.id
-        #return
+        # operations
+        if return_object.user_id.id == 0:
+            if return_object.picking_id.id > 0:
+                if return_object.picking_id.external_stock_picking_id.id > 0:
+                    if return_object.picking_id.external_stock_picking_id.external_source_id.id > 0:
+                        if return_object.picking_id.external_stock_picking_id.external_source_id.external_stock_picking_user_id.id > 0:
+                            return_object.user_id = return_object.picking_id.external_stock_picking_id.external_source_id.external_stock_picking_user_id.id
+        # return
         return return_object
