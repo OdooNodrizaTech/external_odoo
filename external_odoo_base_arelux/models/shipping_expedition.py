5f2c6eb81b8cea4f41627c613243c86e88ced012
Informatica Arelux <informatica@arelux.com>
2020-06-30 11:32:46 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-887

--- external_odoo_base_arelux/models/shipping_expedition.py
+++ external_odoo_base_arelux/models/shipping_expedition.py
@@ -14,8 +14,8 @@ class ShippingExpedition(models.Model):
         if return_object.user_id.id==0:
             if return_object.picking_id.id>0:
                 if return_object.picking_id.external_stock_picking_id.id>0:
-                    if external_stock_picking_id.external_stock_picking_id.external_source_id.id>0:
-                        if external_stock_picking_id.external_stock_picking_id.external_source_id.external_stock_picking_user_id.id>0:
-                            return_object.user_id = external_stock_picking_id.external_stock_picking_id.external_source_id.external_stock_picking_user_id.id
+                    if return_object.picking_id.external_stock_picking_id.external_source_id.id>0:
+                        if return_object.picking_id.external_stock_picking_id.external_source_id.external_stock_picking_user_id.id>0:
+                            return_object.user_id = return_object.picking_id.external_stock_picking_id.external_source_id.external_stock_picking_user_id.id
         #return
         return return_object
