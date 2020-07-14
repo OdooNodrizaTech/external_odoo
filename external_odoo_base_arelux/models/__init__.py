1e8224437104fb776b9e12496bf7dbbd1a4041fa
victoralmau <victoralmau@gmail.com>
2020-07-14 07:42:14 +0200

    reestructuracion

--- external_odoo_base_arelux/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import external_customer
-import external_sale_order
-import external_source
-import external_stock_picking
-import sale_order
-import shipping_expedition
