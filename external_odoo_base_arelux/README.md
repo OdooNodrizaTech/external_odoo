c42d3a9fe4852e57347b1c8b6baeede835257644
victoralmau <victoralmau@gmail.com>
2020-07-14 08:41:26 +0200

    reestructuración

--- external_odoo_base_arelux/README.md
+++
@@ -1,1 +0,0 @@
-Implementa algunas funcionalidades respecto a Arelux para el addon external_odoo_base (arelux/particular y/o auto-etiquetas a los contactos creados)
--- external_odoo_base_arelux/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from . import external_customer
-from . import external_sale_order
-from . import external_source
-from . import external_stock_picking
-from . import sale_order
-from . import shipping_expedition
